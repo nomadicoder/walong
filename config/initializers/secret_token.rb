f434ea999cd8af402442bbe52c0f70f4e56e3eef
Steven Ng <steven@nomadicoder.com>
2013-10-30 20:30:26 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Walong::Application.config.secret_key_base = '097085af1d2437bcfc21a564298b723dbac4f460f4e9ff3cc662fe9d9729be7ee44417060cf4b3cb8a4994189b7860604ebad640b7d10fa9646c9c6e160702a3'
