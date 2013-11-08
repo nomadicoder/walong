dc609cb0e9500f94661015a0df8b9afe866903c3
Steven Ng <steven@nomadicoder.com>
2013-11-08 13:46:40 -0800

    Dont use ActiveFedore Qualified Dublin Core class

--- app/models/datastreams/book_metadata.rb
+++ app/models/datastreams/book_metadata.rb
@@ -1,7 +1,10 @@
-class BookMetadata < ActiveFedora::QualifiedDublinCoreDatastream
+class BookMetadata < ActiveFedora::OmDatastream
   set_terminology do |t|
     t.root(path: "fields")
     t.title(index_as: :stored_searchable)
     t.creator(index_as: :stored_searchable)
+    t.publisher(index_as: :stored_searchable)
+    t.dateCopyrighted(index_as: :stored_searchable)
+    t.abstract(index_as: :stored_searchable)
   end
 end
