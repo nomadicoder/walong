fc43b018d2ab45572abe986ea552f9a8c4ae748d
Steven Ng <steven@nomadicoder.com>
2013-11-08 14:18:58 -0800

    Don't use ActiveFedora Qualified Dublin Core class

--- app/models/datastreams/video_metadata.rb
+++ app/models/datastreams/video_metadata.rb
@@ -1,4 +1,4 @@
-class VideoMetadata < ActiveFedora::QualifiedDublinCoreDatastream
+class VideoMetadata < ActiveFedora::OmDatastream
   set_terminology do |t|
     t.root(path: "fields")
     t.title(index_as: :stored_searchable)
